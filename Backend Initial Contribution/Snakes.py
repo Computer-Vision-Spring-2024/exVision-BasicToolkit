9ddeb96e224b698b3da80b6d11c244a331b35078
Youssef Shawki <ya0721435@gmail.com>
2024-03-31 06:55:12 +0200

    add chain code plus computing area and perimeter of the enclosed contour

--- Backend Initial Contribution/Snakes.py
+++ Backend Initial Contribution/Snakes.py
@@ -15,25 +15,45 @@ processor.apply_filter(image_4, "gaussian", filter_size= 20, sigma = 10 )
 image_4.display()
 
 
-def resample_contour(contour, num_points):
-    # Calculate the cumulative distance along the contour
-    cumulative_distance = np.cumsum(np.sqrt(np.sum(np.diff(contour, axis=0)**2, axis=1)))
+def resample_contour( contour, threshold_distance):
+    resampled_contour = [contour[0]]  
+    current_point = contour[0]
     
-    # Normalize the cumulative distance to range [0, 1]
-    normalized_distance = cumulative_distance / cumulative_distance[-1]
-    
-    # Ensure that normalized_distance has the same length as contour
-    normalized_distance = np.linspace(0, 1, len(contour))
-    
-    # Create an interpolation function for each coordinate
-    interp_func_x = interp1d(normalized_distance, contour[:, 0], kind='cubic')
-    interp_func_y = interp1d(normalized_distance, contour[:, 1], kind='cubic')
-    
-    # Generate evenly spaced samples along the contour
-    t = np.linspace(0, 1, num_points)
-    resampled_contour = np.column_stack((interp_func_x(t), interp_func_y(t)))
-    
-    return resampled_contour
+    for i in range(1, len(contour)):
+        next_point = contour[i]
+        distance = np.sqrt(np.sum((next_point - current_point) ** 2))  
+        if distance >= threshold_distance:
+            num_segments = distance / threshold_distance 
+            if num_segments > 1:
+                for j in range(1, int(num_segments)):
+                    t = j / num_segments
+                    interpolated_point = current_point + t * (next_point - current_point)
+                    resampled_contour.append(interpolated_point)
+                distance= np.sqrt(np.sum((next_point - resampled_contour[-1]) ** 2))
+                if distance > 0.6 * 2* threshold_distance :
+                    midpoint = (resampled_contour[-1] + next_point) / 2
+                    resampled_contour.append(midpoint)
+            resampled_contour.append(next_point)
+            current_point = next_point  
+    distance = np.sqrt(np.sum((resampled_contour[0] - resampled_contour[-1]) ** 2))
+    if distance< threshold_distance:
+        resampled_contour.pop()
+    resampled_contour= resampled_contour[::4]
+    return np.array(resampled_contour)
+
+def plot_contour(ax, contour):
+    ax.clear()
+    ax.imshow(image_3.original_img, cmap='gray')
+    ax.plot(contour[:, 0], contour[:, 1], 'ro-')
+    end_points_x = [contour[0, 0], contour[-1, 0]] 
+    end_points_y = [contour[0, 1], contour[-1, 1]] 
+    ax.plot(end_points_x,end_points_y, 'ro-')
+    ax.axis('off')
+    plt.tight_layout()
+
+
+
+   
 
 plt.ion()
 def onmove(event):
@@ -60,10 +80,8 @@ def onrelease(event):
         if len(contour) > 0:
             # Resample the collected contour
             num_points = len(contour) // 2   # Change this number as needed
-            resampled_contour = resample_contour(contour, num_points)
-
-            ax.plot(resampled_contour[:, 0], resampled_contour[:, 1], 'ro')
-            
+            resampled_contour = resample_contour(contour, 4)
+            plot_contour(ax,resampled_contour)
             contour = np.array(resampled_contour, dtype=int)
 
 
@@ -172,8 +190,7 @@ def update_contour(image_gradient ,contour, window_size ,alpha = 1, beta = 0.5 ,
 
     return  contour 
 
-
-        
+     
     
 window_size = 3
 
@@ -190,10 +207,7 @@ for _ in range(num_iterations):
 
     contour = update_contour(image_4.manipulated_img, contour, window_size, alpha=ALPHA, beta=BETA ,gama = GAMA)
 
-    # Clear and redraw the plot
-    ax.clear()
-    ax.imshow(image_3.original_img, cmap='gray')
-    ax.plot(contour[:, 0], contour[:, 1], 'ro-')
+    plot_contour(ax, contour)
 
     # # Save the current frame
     fig.canvas.draw()
@@ -203,9 +217,83 @@ for _ in range(num_iterations):
 imageio.mimsave('snake_animation.gif', frames, fps=10)
 
 
-fig_2, ax_2 = plt.subplots()
-ax_2.imshow(image_3.original_img, cmap='gray')
-ax_2.plot(contour[:, 0], contour[:, 1], 'ro')
-ax.axis('off')
-plt.tight_layout()
 
+input("Press Enter to Generate the Chain Code")
+
+
+# code_lookup_coordiate_system = {
+#     0 : list(range(339,361)) + list(range(0,23,1)), 
+#     1 : range(23,68), 
+#     2 : range(68,113),  
+#     3 : range(113,158), 
+#     4 : range(158,203), 
+#     5 : range(203, 248), 
+#     6 : range(248, 293), 
+#     7 : range(293,338)
+# }
+
+# tailored specifially 
+# we have used this lookup because the origin the in the image space in the upper left corner. 
+code_lookup_image_space = {
+    0 : list(range(339,361)) + list(range(0,23,1)), 
+    7 : range(23,68), 
+    7 : range(68,113),  
+    5 : range(113,158), 
+    4 : range(158,203), 
+    3 : range(203, 248), 
+    2 : range(248, 293), 
+    1 : range(293,338)
+}
+
+
+
+def compute_chain_code(contour):  
+    if not np.array_equal(contour[0], contour[-1]):
+        contour = np.vstack((contour, contour[0]))
+    
+        
+    chain_code = list()
+
+    for i in range(len(contour[:-1])):
+        dx = contour[i + 1][0] - contour[i][0]
+        dy = contour[i + 1][1] - contour[i][1]
+        slope = round(np.arctan2(dy,dx) * 180/np.pi) # to degrees 
+        if slope < 0: 
+            slope += 360
+        for key,val in code_lookup_image_space.items():
+            if slope in val:
+                chain_code.append(key)
+                break;
+
+    return chain_code
+
+chain_code= compute_chain_code(contour.copy())
+print(chain_code)
+
+input("Press Enter Compute the Area")
+
+# Shoelace formula for computing the area enclosed with a set of points of defined coordinate points
+def compute_area(contour):
+    x = contour[:, 0]
+    y = contour[:, 1]
+    return 0.5 * np.abs(np.dot(x, np.roll(y, -1)) - np.dot(y, np.roll(x, -1)))
+
+area = compute_area(contour)
+print(round(area,3))
+
+# test case (square 2x2)
+# contour_arc = np.array([
+#     [0,0],
+#     [2,0],[2,2],[0,2]])
+
+input("Press Enter Compute the Perimeter")
+
+def compute_perimeter(contour):
+    distances = np.sqrt(np.sum(np.diff(contour, axis=0)**2, axis=1))
+    perimeter = np.sum(distances) + np.linalg.norm(contour[-1] - contour[0]) # adding the Eucliden distance between the first and last points
+    return perimeter
+
+perimeter = compute_perimeter(contour)
+print(round(perimeter,3))
+
+input("Done!!")
